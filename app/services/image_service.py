ddee680474926706ec9bcac093129689fee0b39b
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2025-02-18 21:29:04 +0200

    Update image_service.py

--- app/services/image_service.py
+++ app/services/image_service.py
@@ -1,14 +1,18 @@
 # itegration with Cloudinary, image processing, upload images to the cloudinary
 import cloudinary
-from app.database.models import Image
-from app.services.auth_service import get_current_user
-from app.database.connection import AsyncSession
-from app.templates.config import settings
-from app.services.auth_service import RoleSet
 from cloudinary.uploader import upload
-from app.templates.schemas import ImageCreate
+import cloudinary.api
 from typing import List
 from fastapi import HTTPException
+
+from app.database.models import Image, User
+from app.services.auth_service import get_current_user #уточнить импорт!
+from app.database.connection import  get_conn_db
+from app.templates.config import settings
+from app.services.auth_service import 
+
+from app.templates.schemas import ImageCreate
+
 
 # Настройка Cloudinary
 cloudinary.config(
@@ -17,11 +21,11 @@ cloudinary.config(
     api_secret=settings.CLD_API_SECRET  
 )
 
-async def upload_image_service(description: str,
-                               file,  # UploadFile
-                               tags: List[str], 
-                               db: AsyncSession,
-                               current_user) -> dict:
+async def upload_file(description: str,
+                      file: UploadFile = File(...),
+                      tags: Optional[List[str]] = Query([]),
+                      db: AsyncSession = Depends(get_conn_db),
+                      current_user: User = Depends(get_current_user)) -> dict:
     # download image cloud cloudinary
     try:
         result = cloudinary.uploader.upload(file.file, folder=current_user.email)
