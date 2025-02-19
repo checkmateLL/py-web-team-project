6a49af11c91e3c8fd737afbd16af50353621aad9
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2025-02-19 09:46:05 +0200

    Update images.py

--- app/repository/images.py
+++ app/repository/images.py
@@ -1,1 +1,68 @@
-# crud images operations
+# crud images operations with db
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from fastapi import HTTPException
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+from app.database.models import Image, User
+from app.schemas import ImageResponseSchema
+
+
+async def update_image_description(image_id: int, description: str, db: AsyncSession, current_user: User) -> ImageResponseSchema:
+    try:
+      
+        result = await db.execute(select(Image).filter(Image.id == image_id))
+        image = result.scalar_one_or_none()
+
+        if image is None:
+            raise HTTPException(status_code=404, detail="Image not found.")
+
+        if image.owner_id != current_user.id:
+            raise HTTPException(status_code=403, detail="You don't have permission to update this image.")
+
+        # Update the image description
+        image.description = description
+        await db.commit()
+        await db.refresh(image)
+
+        return ImageResponseSchema(url=image.url, description=image.description, owner_id=image.owner_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+async def get_image_by_url(image_url: str, db: AsyncSession) -> ImageResponseSchema:
+    try:
+        
+        result = await db.execute(select(Image).filter(Image.url == image_url))
+        image = result.scalar_one_or_none()
+
+        if image is None:
+            raise HTTPException(status_code=404, detail="Image not found.")
+
+        return ImageResponseSchema(url=image.url, description=image.description, owner_id=image.owner_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    
+async def delete_image(image_id: int, db: AsyncSession, current_user: User):
+    try:
+        
+        result = await db.execute(select(Image).filter(Image.id == image_id))
+        image = result.scalar_one_or_none()
+
+        if image is None:
+            raise HTTPException(status_code=404, detail="Image not found.")
+        
+        if image.user_id != current_user.id:
+            raise HTTPException(status_code=403, detail="You don't have permission to delete this image.")
+        
+        # Delete image from Cloudinary
+        cloudinary.uploader.destroy(image.public_id)#при удалении фото из cloudinary вносить поле public_id в модель+migrations!
+
+        # Delete image record from database
+        await db.delete(image)
+        await db.commit()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
